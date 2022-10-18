3d476c8a0a4c8a343f4ea3c07ff0c9cf6ec448a9
Artica08 <87441067+Artica08@users.noreply.github.com>
2022-10-18 14:54:09 +0200

    Add project files.

---
+++ opencv_test/opencv_test.cpp
@@ -0,0 +1,31 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	/*Mat image = Mat::zeros(300, 600, CV_8UC3);
+	circle(image, Point(250, 150), 100, Scalar(0, 255, 128), -100);
+	circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
+	imshow("Display Window", image);
+	waitKey(0);
+	return 0;*/
+    std::string image_path = samples::findFile("starry_night.jpg");
+    Mat img = imread(image_path, IMREAD_COLOR);
+    if (img.empty())
+    {
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return 1;
+    }
+    imshow("Display window", img);
+    int k = waitKey(0); // Wait for a keystroke in the window
+    if (k == 's')
+    {
+        imwrite("starry_night.png", img);
+    }
+    return 0;
+}
